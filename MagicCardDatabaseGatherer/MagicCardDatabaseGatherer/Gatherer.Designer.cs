0840ccbdd23a07bc7e167d02a7f7b441b2da6021
Luiz Henrique Strobelt Pérez <lhsperez@gmail.com>
2013-09-21 02:49:56 -0300

    Ordering List and using it to read the cards from the gatherer. Also updated UI

--- MagicCardDatabaseGatherer/MagicCardDatabaseGatherer/Gatherer.Designer.cs
+++ MagicCardDatabaseGatherer/MagicCardDatabaseGatherer/Gatherer.Designer.cs
@@ -33,18 +33,24 @@
             this.lblProgress = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.lblTimer = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.cardListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.orderListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // progressBar
             // 
-            this.progressBar.Location = new System.Drawing.Point(14, 63);
+            this.progressBar.Location = new System.Drawing.Point(14, 79);
             this.progressBar.Name = "progressBar";
             this.progressBar.Size = new System.Drawing.Size(497, 31);
             this.progressBar.TabIndex = 0;
             // 
             // btnConvert
             // 
-            this.btnConvert.Location = new System.Drawing.Point(344, 12);
+            this.btnConvert.Location = new System.Drawing.Point(344, 28);
             this.btnConvert.Name = "btnConvert";
             this.btnConvert.Size = new System.Drawing.Size(166, 45);
             this.btnConvert.TabIndex = 2;
@@ -56,8 +62,9 @@
             // 
             this.lblProgress.AutoSize = true;
             this.lblProgress.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblProgress.Location = new System.Drawing.Point(265, 12);
+            this.lblProgress.Location = new System.Drawing.Point(136, 0);
             this.lblProgress.Name = "lblProgress";
+            this.lblProgress.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.lblProgress.Size = new System.Drawing.Size(73, 44);
             this.lblProgress.TabIndex = 3;
             this.lblProgress.Text = "0/0";
@@ -65,7 +72,7 @@
             // textBox1
             // 
             this.textBox1.AllowDrop = true;
-            this.textBox1.Location = new System.Drawing.Point(13, 100);
+            this.textBox1.Location = new System.Drawing.Point(13, 116);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
@@ -80,18 +87,62 @@
             this.lblTimer.Size = new System.Drawing.Size(0, 13);
             this.lblTimer.TabIndex = 4;
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cardListToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(522, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // cardListToolStripMenuItem
+            // 
+            this.cardListToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.orderListToolStripMenuItem});
+            this.cardListToolStripMenuItem.Name = "cardListToolStripMenuItem";
+            this.cardListToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.cardListToolStripMenuItem.Text = "Card List";
+            // 
+            // orderListToolStripMenuItem
+            // 
+            this.orderListToolStripMenuItem.Name = "orderListToolStripMenuItem";
+            this.orderListToolStripMenuItem.Size = new System.Drawing.Size(125, 22);
+            this.orderListToolStripMenuItem.Text = "Order List";
+            this.orderListToolStripMenuItem.Click += new System.EventHandler(this.orderListToolStripMenuItem_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.lblProgress, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(126, 28);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(212, 44);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
             // Gatherer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(522, 392);
+            this.ClientSize = new System.Drawing.Size(522, 409);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.lblTimer);
-            this.Controls.Add(this.lblProgress);
             this.Controls.Add(this.btnConvert);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Gatherer";
             this.Text = "Gatherer";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -104,6 +155,10 @@
         private System.Windows.Forms.Label lblProgress;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label lblTimer;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem cardListToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem orderListToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
 
--- MagicCardDatabaseGatherer/MagicCardDatabaseGatherer/Gatherer.cs
+++ MagicCardDatabaseGatherer/MagicCardDatabaseGatherer/Gatherer.cs
@@ -10,6 +10,8 @@ using System.Windows.Forms;
 using BlackLotus.Cards;
 using System.Diagnostics;
 using System.Threading;
+using System.IO;
+using Helpers;
 
 namespace WindowsFormsApplication1
 {
@@ -21,6 +23,7 @@ namespace WindowsFormsApplication1
         private delegate void SetProgressDelegate(int value);
         private delegate void AddProgressDelegate(int value);
         private delegate void ShowElapsedTimeDelegate();
+        private delegate void UpdateCardCounterDelegate();
         CardGather cardGather;
         List<int?> _cardsToGather = new List<int?>();
         ThreadedQueue<int?> queue;
@@ -29,26 +32,56 @@ namespace WindowsFormsApplication1
 
         public Gatherer()
         {
-            for (int i = 0; i < 100; i++)
-            {
-                _cardsToGather.Add(240034 + i);
-            }
             cardGather = new CardGather(this);
             InitializeComponent();
-            progressBar.Maximum = _cardsToGather.Count;
-            queue = new ThreadedQueue<int?>(this, _cardsToGather);
             _stopwatch = new Stopwatch();
         }
 
         private void btnConvert_Click(object sender, EventArgs e)
         {
-            _stopwatch.Start();
-            queue.Start(16);
+            StreamReader reader;
+            //  Check if the cardIds text we need exists
+            if (!File.Exists("OrderedCardIds.txt"))
+            {
+                MessageBox.Show("Please first order the card list.");
+            }
+            else
+            {
+                //  Reads the ids we want to get from the Magic Gatherer
+                reader = new StreamReader(File.OpenRead("OrderedCardIds.txt"));
+                string fileContent = reader.ReadToEnd();
+                List<string> cardIds = fileContent.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                _cardsToGather = cardIds.Select(GetNullableInt).ToList();
+                progressBar.Maximum = _cardsToGather.Count;
+                queue = new ThreadedQueue<int?>(this, _cardsToGather);
+
+                _stopwatch.Start();
+                queue.Start(16);
+            }
+        }
+
+        private int? GetNullableInt(string value)
+        {
+            return value.GetValueOrNull<int>();
+        }
+
+        private void SetCardCounter()
+        {
+            lblProgress.Text = progressBar.Value.ToString() + '/' + progressBar.Maximum.ToString();
         }
 
         private void Log(string value)
         {
             textBox1.Text = value + "\r\n" + textBox1.Text;
+        }
+
+        private void UpdateCardCounter()
+        {
+            if (this.lblProgress.InvokeRequired)
+                this.lblProgress.Invoke(new UpdateCardCounterDelegate(SetCardCounter));
+            else
+                SetCardCounter();
         }
 
         private void ShowElapsedTime()
@@ -59,7 +92,7 @@ namespace WindowsFormsApplication1
             }
             else
             {
-                lblTimer.Text = _stopwatch.Elapsed.ToString();
+                lblTimer.Text = _stopwatch.Elapsed.ToString(@"hh\:mm\:ss");
             }
         }
 
@@ -107,12 +140,13 @@ namespace WindowsFormsApplication1
             AddProgress(1);
             InsertLog(card.ToString());
             ShowElapsedTime();
+            UpdateCardCounter();
             Application.DoEvents();
         }
 
         void ThreadedQueueViewee<int?>.OnWork(ThreadedQueue<int?> caller, int? item)
         {
-                cardGather.ReadCard(item.Value);
+            cardGather.ReadCard(item.Value);
         }
 
         void ThreadedQueueViewee<int?>.OnWorkFinished(ThreadedQueue<int?> caller, DateTime startTime, DateTime endTime)
@@ -126,6 +160,45 @@ namespace WindowsFormsApplication1
             if (_stopwatch.IsRunning)
             {
                 _stopwatch.Stop();
+            }
+        }
+
+        private void orderListToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(OrderCardIdList().ToString());
+        }
+
+        public bool OrderCardIdList()
+        {
+            try
+            {
+                //  Open the file with the cards ids to read and sort.
+                StreamReader reader = new StreamReader(File.OpenRead("CardIds.txt"));
+                //  Create/Overwrite the file to where the cards ids qill be saved.
+                StreamWriter writer = new StreamWriter(File.Create("OrderedCardIds.txt"));
+                //  Read all the cards ids.
+                string fileContent = reader.ReadToEnd();
+                //  Split the filecontent by line break and ignore empty entries to be sure.
+                //  Each card id must be on its own line.
+                List<string> cardIds = fileContent.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                //  Sort the distinct elements of the list and save them to a list of integers.
+                List<int> convertedCardIds = cardIds.Select(Int32.Parse).Distinct().OrderBy(x => x).ToList();
+                //  Write to the ordered file.
+                //  One Card Id by line.
+                foreach (int cardId in convertedCardIds)
+                    writer.WriteLine(cardId);
+                //  Guarantees the ids have all been written.
+                writer.Flush();
+                //  Disposes of the reader
+                reader.Close();
+                //  Disposes of the writer
+                writer.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                return false;
             }
         }
     }
